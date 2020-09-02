758c9f3a4911ead9f6c1977d93dd0d1c33589819
Mr.T-77 <MR-T77@users.noreply.github.com>
2020-09-02 11:29:29 +0100

    final

--- README.md
+++ README.md
@@ -1,5 +1,32 @@
 # ShiftIfYouCan
 
-Code Repository for ISMIR 2020 LBD Contribution "Shift if you can: Counting and visualising correction operations for beat tracking evaluation".
+Code Repository for ISMIR 2020 LBD Contribution "Shift if you can: Counting and visualising correction operations for beat tracking evaluation".  
+We provide a straightforward calculation of *annotation efficiency* based on counting the number of shifts, insertions, and deletions, and pair it with an informative visualisation suited for the qualitative evaluation of beat tracking systems. It graphically displays the minimum set and type of operations required to transform a sequence of initial beat detections in such a way as to maximize the F-measure when comparing the transformed detections against a ground truth annotation sequence.
 
---To be updated soon--
+## Installation and Usage
+
+To use this library, the user should first clone this repository to the working python directory.
+
+The library can be imported with `...`.
+
+## Dependencies:
+
+* Scipy/Numpy
+
+## Example Code
+
+A detailed example on how to use the TIV.lib is available in [ShiftIfYouCan_example.ipynb](ShiftIfYouCan_example.ipynb).
+
+### 3rd Party Code
+
+On the examples provided we used and/or adapted the following two external functions, which can be found along all relevant information in the module *ext_libraries.py*.
+- **variations**, that provides variations of a given beat sequence; 
+- **f-measure**, that provides the f-measure of a given pair of beat detections and annotations.
+
+## Authors
+
+António Sá Pinto
+antoniosapinto@gmail.com
+
+Matthew E. P. Davies
+mepdavies@gmail.com
---
+++ example1.py
@@ -0,0 +1,61 @@
+"""
+This script provides an example of how to create a list of variations of beat detections,
+and process it with the corresponding ground-truth annotation.
+For each of the variations, it calculates the following:
+  - annotation efficiency;
+  - f-measure;
+  - transformed beat detections and corresponding f-measure;
+Saves the transformed beat detections as well as all the plots as *.png files in the /figures/ folder.
+These figures are rendered as single plots with the reference annotations on the positive part of
+the y-axis and the different operations (shifts, insertions, deletions) on the negative part.
+"""
+
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+from ext_libraries import f_measure, variations
+
+from operating import operation_count, process_operations, get_summary
+from plotting import plot_operations
+
+# Load beat detections and annotations
+dets_file = 'dets'
+dets = np.loadtxt(f'{dets_file}.txt')
+anns = np.loadtxt('hains006.beats')
+
+# Use only the first column (i.e. the time stamp) if these are 2D
+if dets.ndim > 1:
+    dets = dets[:, 0]
+
+if anns.ndim > 1:
+    anns = anns[:, 0]
+
+# Process all variations of beat detections and the ground truth annotations
+dets_variations, types_variations = variations(dets, offbeat=True, double=True, half=True, triple=True, third=True)
+
+# Cycle through the full list of variations
+for dets_variation, type_variation in zip(dets_variations, types_variations):
+
+    # Get matrix of operations and annotation efficiency
+    ops, ann_eff = operation_count(dets_variation, anns)
+
+    # Get list of transformed detections
+    transformed = process_operations(ops)
+
+    # Save list of transformed detections
+    np.savetxt(f'dets_{type_variation}_transformed.txt', transformed, fmt='%.2f')
+
+    # Get combined f-measure (tuple with initial f-measure and transformed f-measure)
+    comb_f_measure = f_measure(dets_variation, anns), f_measure(transformed, anns)
+
+    # Display results
+    print(get_summary(type_variation, ann_eff, comb_f_measure))
+
+    # Get the figure and save it
+    fig, ax = plot_operations(ops, anns, type_variation, plot_type='single')
+    plt.savefig(f'figures/{type_variation}_vis.png', bbox_inches='tight')
+
+    # Show the plot
+    # plt.tight_layout()
+    # plt.show()
